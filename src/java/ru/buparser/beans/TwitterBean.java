7940363f0c516a48554915bcdb57dc9726b9e76c
BIG_bu <BIG_bu@DARK_COMP.KB83.NET>
2013-09-02 10:12:02 +0400

    Added NullPointerException check

--- src/java/ru/buparser/beans/TwitterBean.java
+++ src/java/ru/buparser/beans/TwitterBean.java
@@ -87,86 +87,92 @@ public class TwitterBean implements Serializable{
         
     }
     private void dbInsertUserInfo() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{
-        String insertUser1 = ""
-                 + " INSERT INTO twitterlenta_history (id, date, tweets, followings, followers, parse_time)"
-                 + " SELECT id, lastparse, tweets, followings, followers, parse_time FROM twitterlenta WHERE id= ? ";
-        String insertUser2 = ""
-                 + " INSERT INTO twitterlenta (id,tweets,followings,followers,parse_time)"
-                 + " VALUES ( ? , ? , ? , ? , ?)"
-                 + " ON DUPLICATE KEY UPDATE "
-                 + " tweets    = ? ,"
-                 + " followings= ? ,"
-                 + " followers = ?,"
-                 + " parse_time= ? ";
+        if (user == null ) {
+            throw new NullPointerException("user is null");
+        } else {
+            String insertUser1 = ""
+                     + " INSERT INTO twitterlenta_history (id, date, tweets, followings, followers, parse_time)"
+                     + " SELECT id, lastparse, tweets, followings, followers, parse_time FROM twitterlenta WHERE id= ? ";
+            String insertUser2 = ""
+                     + " INSERT INTO twitterlenta (id,tweets,followings,followers,parse_time)"
+                     + " VALUES ( ? , ? , ? , ? , ?)"
+                     + " ON DUPLICATE KEY UPDATE "
+                     + " tweets    = ? ,"
+                     + " followings= ? ,"
+                     + " followers = ?,"
+                     + " parse_time= ? ";
 
-        Connection conn = null;
-        PreparedStatement pst1 = null;
-        PreparedStatement pst2 = null;
+            Connection conn = null;
+            PreparedStatement pst1 = null;
+            PreparedStatement pst2 = null;
 
-        try {
-            conn = ds.getConnection(ResourceBundle.getBundle("/dbconnect").getString("user"), ResourceBundle.getBundle("/dbconnect").getString("password"));
-            conn.setAutoCommit(false);
-            pst1 = conn.prepareStatement(insertUser1);
-                pst1.setLong(1, user.getId());
-                pst1.executeUpdate();
-            pst2 = conn.prepareStatement(insertUser2);
-                pst2.setLong(1, user.getId());
-                pst2.setInt( 2, user.getStatusesCount());
-                pst2.setInt( 3, freindsIdsCount);
-                pst2.setInt( 4, user.getFollowersCount());
-                pst2.setLong(5, (System.currentTimeMillis()-startParseTime) );
-                pst2.setInt( 6, user.getStatusesCount());
-                pst2.setInt( 7, freindsIdsCount);
-                pst2.setInt( 8, user.getFollowersCount());
-                pst2.setLong(9, (System.currentTimeMillis()-startParseTime) );
-                pst2.executeUpdate();
-            conn.commit();
-                
-        } finally {
-            if (pst1 != null) { pst1.close(); }
-            if (pst2 != null) { pst2.close(); }
-            if (conn != null) { conn.close(); }
-        }
-            
+            try {
+                conn = ds.getConnection(ResourceBundle.getBundle("/dbconnect").getString("user"), ResourceBundle.getBundle("/dbconnect").getString("password"));
+                conn.setAutoCommit(false);
+                pst1 = conn.prepareStatement(insertUser1);
+                    pst1.setLong(1, user.getId());
+                    pst1.executeUpdate();
+                pst2 = conn.prepareStatement(insertUser2);
+                    pst2.setLong(1, user.getId());
+                    pst2.setInt( 2, user.getStatusesCount());
+                    pst2.setInt( 3, freindsIdsCount);
+                    pst2.setInt( 4, user.getFollowersCount());
+                    pst2.setLong(5, (System.currentTimeMillis()-startParseTime) );
+                    pst2.setInt( 6, user.getStatusesCount());
+                    pst2.setInt( 7, freindsIdsCount);
+                    pst2.setInt( 8, user.getFollowersCount());
+                    pst2.setLong(9, (System.currentTimeMillis()-startParseTime) );
+                    pst2.executeUpdate();
+                conn.commit();
+
+            } finally {
+                if (pst1 != null) { pst1.close(); }
+                if (pst2 != null) { pst2.close(); }
+                if (conn != null) { conn.close(); }
+            }
+        }        
     }
 
     private void dbInsertStatuses() throws SQLException{
-        String insertStatuses = ""
-                 + " INSERT INTO twitterlenta_tweets (id, user_id, text, retweet_count, created_at)"
-                 + " VALUES (?, ?, ?, ?, ?)"
-                 + " ON DUPLICATE KEY UPDATE user_id = VALUES(user_id)";
+        if (statuses == null ) {
+            throw new NullPointerException("tweets is null");
+        } else {
+            String insertStatuses = ""
+                     + " INSERT INTO twitterlenta_tweets (id, user_id, text, retweet_count, created_at)"
+                     + " VALUES (?, ?, ?, ?, ?)"
+                     + " ON DUPLICATE KEY UPDATE user_id = VALUES(user_id)";
 
-        Connection conn = null;
-        PreparedStatement pst = null;
+            Connection conn = null;
+            PreparedStatement pst = null;
 
-        try {
-            conn = ds.getConnection(ResourceBundle.getBundle("/dbconnect").getString("user"), ResourceBundle.getBundle("/dbconnect").getString("password"));
-            conn.setAutoCommit(false);
-            pst = conn.prepareStatement(insertStatuses);
-            int i = 0;
-                    for(Status status : statuses ){
-                     java.sql.Timestamp  sqlCreatedAt  = new java.sql.Timestamp(status.getCreatedAt().getTime());
-                        pst.setLong(1, status.getId());
-                        pst.setLong(2, status.getUser().getId());
-                        pst.setString(3, status.getText());
-                        pst.setLong(4, status.getRetweetCount());
-                        pst.setTimestamp(5, sqlCreatedAt);
+            try {
+                conn = ds.getConnection(ResourceBundle.getBundle("/dbconnect").getString("user"), ResourceBundle.getBundle("/dbconnect").getString("password"));
+                conn.setAutoCommit(false);
+                pst = conn.prepareStatement(insertStatuses);
+                int i = 0;
+                        for(Status status : statuses ){
+                         java.sql.Timestamp  sqlCreatedAt  = new java.sql.Timestamp(status.getCreatedAt().getTime());
+                            pst.setLong(1, status.getId());
+                            pst.setLong(2, status.getUser().getId());
+                            pst.setString(3, status.getText());
+                            pst.setLong(4, status.getRetweetCount());
+                            pst.setTimestamp(5, sqlCreatedAt);
 
-                        pst.addBatch();
+                            pst.addBatch();
 
-                        if( ((i + 1) % 1000) == 0 ) {pst.executeBatch();} // выполнить запрос партиями по 1000 записей
-                    }
-                pst.executeBatch();
-            conn.commit();
-                
-        } catch (SQLException e) {
-            conn.rollback();
-            throw e;
-        }
-        finally {
-            if (pst  != null) { pst.close(); }
-            if (conn != null) { conn.close(); }
-        }
-       
+                            if( ((i + 1) % 1000) == 0 ) {pst.executeBatch();} // выполнить запрос партиями по 1000 записей
+                        }
+                    pst.executeBatch();
+                conn.commit();
+
+            } catch (SQLException e) {
+                conn.rollback();
+                throw e;
+            }
+            finally {
+                if (pst  != null) { pst.close(); }
+                if (conn != null) { conn.close(); }
+            }
+            }   
     }
 }
